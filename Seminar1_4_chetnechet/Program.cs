34e9e4f4aa2583df798f6af3093a2962d584737e
Just_sound <just_sound@mail.ru>
2023-01-13 21:55:29 +0300

    Добавили определение четности

---
+++ Seminar1_4_chetnechet/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+  if (a%2 == 0){
+    Console.Write("Число ");
+    Console.Write(a);
+    Console.Write(" чётное.");
+  }
+  if (a%2 > 0){
+    Console.Write("Число ");
+    Console.Write(a);
+    Console.Write(" нечётное.");
+  }
