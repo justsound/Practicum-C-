5c55ea1a1ac0bbfa6b105c65d26b0e4a4571af15
Just_sound <just_sound@mail.ru>
2023-01-13 21:12:55 +0300

    Добавили пример на нахождение максимума и минимума

---
+++ Seminar1_2_MinMax/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите целое первое число ");
+int number1 = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int number2 = Convert.ToInt32( Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Максимальное число ");
+    Console.WriteLine(number1);
+    Console.WriteLine("Минимальное число ");
+    Console.WriteLine(number2);
+}
+else
+{
+     Console.WriteLine("Максимальное число ");
+    Console.WriteLine(number2);
+    Console.WriteLine("Минимальное число ");
+    Console.WriteLine(number1);
+};
