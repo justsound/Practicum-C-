19b135a038f2b7dbe3e6a7ae27073b01478f084c
Just_sound <just_sound@mail.ru>
2023-01-13 21:39:11 +0300

    Добавили нахождение максимума из трех чисел

---
+++ Seminar1_3_maximum/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите целое первое число ");
+int number1 = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int number2 = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите третье целое число ");
+int number3 = Convert.ToInt32( Console.ReadLine());
+
+int max = number1;
+
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
