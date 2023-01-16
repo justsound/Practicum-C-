2893f592ba0b6f38f7b6eb057c31c4ebebdf2a49
Just_sound <just_sound@mail.ru>
2023-01-16 17:21:02 +0300

    Добавили первый массив

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {11, 22, 33, 44, 15, 56, 47, 28, 19};
+
+int max = Max(
+    Max(array[0], array[2], array[1]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
