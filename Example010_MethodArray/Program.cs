09ead058c226b9f7cbe78292afe92ace8415f3d4
Just_sound <just_sound@mail.ru>
2023-01-16 17:28:12 +0300

    Добавили метод массив

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {17,2,3,4,52,546,17,81};
+
+int n = array.Length;
+
+int find = 17;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
